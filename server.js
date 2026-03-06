bbda19a668b0e71a193d60061a77b1d18b0e87cd
ravan-220 <rahulktp155@gmail.com>
2026-03-07 05:29:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 3000;
 
-const PORT = process.env.PORT || 3000;
+// Static files serve karein
+app.use(express.static(path.join(__dirname)));
 
-// 'public' folder ki files ko serve karne ke liye
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Main route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
